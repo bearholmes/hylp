3a6264330d14f769f73ebb722bf1a9dfc4312fb6
bearholmes <bearholmes@gmail.com>
2017-02-20 20:01:11 +0900

    코드가 좀 엉망입니다. 1-2년차 보정인상율 계산 추가

--- public/js/calc_common.js
+++ public/js/calc_common.js
@@ -55,7 +55,7 @@ NumberValidator = (function() {
         while (reg.test(n)) n = n.replace(reg, '$1' + ',' + '$2');
         return n;
     }
-    
+
     return {
         initialize: function(e) {
             var elements = doc.getElementsByClassName("number_validation");
@@ -82,15 +82,15 @@ function calc_01() {
 		alert("전년 입력필드에 값을 입력해주세요.");
 		before.focus();
 		return false;
-	} 
+	}
 	if (after.value == "") {
 		alert("올해 입력필드에 값을 입력해주세요.");
 		after.focus();
 		return false;
-	}	
+	}
 	var result_value = ((after_value-before_value)/before_value)*100;
-	
-	
+
+
 	var output = '<div class="info_calc"><span class="tit_base">전년 대비</span>';
 		output +='<span class="txt_num"><em class="txt_emph">'+result_value.toFixed(1)+'</em><span class="txt_unit">&nbsp;%</span></span></div>';
 	$("msg_01").innerHTML = output;
@@ -102,7 +102,7 @@ function calc_02() {
 		alert("기준연봉 입력필드에 값을 입력해주세요.");
 		basis.focus();
 		return false;
-	} 
+	}
 
 	function conv_cost(cost, rate){
 		var out_value = ((cost*(100+rate))/100).toFixed(0);
@@ -117,7 +117,7 @@ function calc_02() {
 
 	var output = '<table><caption class="screen_out">기준연봉대비 구간예상</caption>';
 		output += '<thead><tr><th>인상율</th><th>예상 금액</th></tr></thead>';
-	for (var i=1; i<7; i++){	
+	for (var i=1; i<7; i++){
 		output += '<tbody><tr><td>'+rate[i]+'%</td><td>'+ NumberValidator.insertComma(rate_cost[i]) +' 원</td></tr>';
 	}
 		output += '</tbody></table>';
@@ -131,7 +131,7 @@ function calc_03() {
 		alert("시작 연봉 입력필드에 값을 입력해주세요.");
 		basis.focus();
 		return false;
-	} 
+	}
 	if (month.value == "") {
 		alert("근무 개월 입력필드에 값을 입력해주세요.");
 		month.focus();
@@ -156,7 +156,7 @@ function calc_03() {
 
 	var output = '<table><caption class="screen_out">기준연봉대비 구간예상</caption>';
 		output += '<thead><tr><th>인상율</th><th>보정</th><th>예상 금액</th></tr></thead>';
-	for (var i=1; i<7; i++){	
+	for (var i=1; i<7; i++){
 		output += '<tbody><tr><td>'+rate[i]+'%</td><td>'+rate_corr[i]+'%</td><td>'+ NumberValidator.insertComma(rate_cost[i]) +' 원</td></tr>';
 	}
 		output += '</tbody></table>';
@@ -175,7 +175,7 @@ function calc_04() {
 		alert("평균 인상율 입력필드에 값을 입력해주세요.");
 		percent.focus();
 		return false;
-	} 
+	}
 
 	function conv_cost(cost, percents){
 		var out_value = ((cost*(100+percents))/100).toFixed(0);
@@ -189,15 +189,15 @@ function calc_04() {
 	}
 	var year = new Array();
 	var month = new Array();
-	
+
 	year[0] = basis_value;
 	month[0] = conv_after_cost(year[0]);
-	
+
 	for (var i=1; i<10; i++){
 		year[i] = conv_cost(year[i-1], percent_value);
 		month[i] = conv_after_cost(year[i]);
 	}
-	
+
 	var output = '<table><caption class="screen_out">예상연봉 결과표</caption>';
 		output += '<thead><tr><th>년도</th><th>예상 연봉</th><th>예상 월급 (세후)</th></tr></thead><tbody>';
 		output += '<tr><td>0</td><td>'+ NumberValidator.insertComma(year[0]) +'원</td><td>'+ NumberValidator.insertComma(month[0]) +'원</td></tr>';
@@ -209,11 +209,47 @@ function calc_04() {
 	$("msg_04").innerHTML = output;
 	$("msg_04").style.display = "block";
 };
+function calc_05() {
+	var before = $("calc_inp_08"), before_value = parseInt(before.value.replaceAll(",", ""));
+	var after = $("calc_inp_09"), after_value = parseInt(after.value.replaceAll(",", ""));
+	var month = $("calc_inp_10"), month_value = parseInt(month.value.replaceAll(",", ""));
+	if (before.value == "") {
+		alert("기준 연봉 입력필드에 값을 입력해주세요.");
+		before.focus();
+		return false;
+	}
+	if (after.value == "") {
+		alert("예상 연봉 입력필드에 값을 입력해주세요.");
+		after.focus();
+		return false;
+	}
+	if (month.value == "") {
+		alert("근무 개월 입력필드에 값을 입력해주세요.");
+		month.focus();
+		return false;
+	}
+	if (month.value < 7 || month.value > 24) {
+		alert("7~23개월 사이로 입력해주세요.");
+		month.focus();
+		return false;
+	}
+
+	var ra_cost = ((after_value - before_value)/before_value) *100;
+	var cy_cost = ((((after_value - before_value)/month_value)*12)/before_value) *100;
+
+	var output = '<div class="info_calc"><span class="tit_base">예상 인상율 (보정 인상율)</span>';
+		output +='<span class="txt_num"><em class="txt_emph">'+ra_cost.toFixed(2)+' ('+ cy_cost.toFixed(2)+')</em><span class="txt_unit">&nbsp;%</span></span></div>';
+
+	$("msg_05").innerHTML = output;
+	$("msg_05").style.display = "block";
+};
+
 
 $("btn_01").addEventListener("click", calc_01);
 $("btn_02").addEventListener("click", calc_02);
-$("btn_03").addEventListener("click", calc_03);	
+$("btn_03").addEventListener("click", calc_03);
 $("btn_04").addEventListener("click", calc_04);
+$("btn_05").addEventListener("click", calc_05);
 (function($) {
 	NumberValidator.initialize("calculatorColl");
 })();
